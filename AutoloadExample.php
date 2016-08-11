cc80ca14a773e2010c0da2134db5016989285f8b
root <root@bdwk.ru>
2016-08-11 23:11:33 +0300

    initial

---
+++ AutoloadExample.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace khalmeev\rest;
+
+/**
+ * This is just an example.
+ */
+class AutoloadExample extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
